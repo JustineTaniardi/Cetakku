669aba3f933afab72d214711c77a151f955c89ca
JustineTaniardi <TANIARDIJUSTINE@GMAIL.COM>
2026-02-03 09:41:19 +0700

    fix controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,13 +13,18 @@ Route::middleware(['auth', 'role:admin'])->group(function (){
         return 'admin page';
     })->name('admin');
 });
+
 Route::middleware(['auth', 'role:kasir'])->group(function (){
     Route::get('/kasir', function (){
         return 'kasir page';
     })->name('kasir');
 });
+
 Route::middleware(['auth', 'role:pekerja'])->group(function (){
     Route::get('/pekerja', function (){
         return 'pekerja page';
     })->name('pekerja');
 });
+
+Route::get('/login', [AuthController::class, 'index']);
+Route::post('/login', [AuthController::class, 'login']);

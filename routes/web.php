f8703b7cb9dfffc7bf4698c5bdbfbfa8d8a44198
JustineTaniardi <TANIARDIJUSTINE@GMAIL.COM>
2026-02-03 09:03:52 +0700

    middleware

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::middleware(['auth', 'role:admin'])->group(function (){
+    Route::get('/admin', function (){
+        return 'admin page';
+    })->name('admin');
+});
+Route::middleware(['auth', 'role:kasir'])->group(function (){
+    Route::get('/kasir', function (){
+        return 'kasir page';
+    })->name('kasir');
+});
+Route::middleware(['auth', 'role:pekerja'])->group(function (){
+    Route::get('/pekerja', function (){
+        return 'pekerja page';
+    })->name('pekerja');
+});
